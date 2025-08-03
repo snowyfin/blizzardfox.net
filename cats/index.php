cf739d725aaa0284bf98dc55fd65aee8321a26bb
Ashlyn <patrickfinnconroy@gmail.com>
2025-08-03 13:54:42 -0400

    Update Cat Push

--- cats/index.php
+++ cats/index.php
@@ -16,18 +16,19 @@
     </div>
     <script src="/!g/oneko.js"></script>
     <div id="container">
-      <div class="box" style="box-shadow: 0px 0px 40px 2px white; width:250px;text-align: center;margin-left:auto;margin-right:auto;">
-        Welcome to to the wall of cats, I'll try to add as many photos I take here of my cats :3
-      </div>
-      <div class="gallery">
+      <div class="dcordchannel">Cat Wall<div>Welcome to to the wall of cats, I'll add as many photos I take here of my cats :3</div></div>
+      <div class='dcord2'>
+
         <?php 
         $dirname = "./!f/";
         $images = glob($dirname."*.jpg");
           
         foreach($images as $image) {
-              echo '<div class="box" style="text-align: center;"><img src="'.$image.'"></div>';
+              echo '<div><user style='--icon: url(https://cdn.discordapp.com/avatars/741823940061560944/8730cfd2557617a015a8cb776317f2a8?size=1024)'>Ashlyn</user></div>
+      <div><img src="'.$image.'"></div>';
         }
         ?>
+      
       </div>
     </div>  
   </body>
