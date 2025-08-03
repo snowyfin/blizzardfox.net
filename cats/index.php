4d7a75e5498584ba37806005c731c33ae9953af9
Ashlyn <patrickfinnconroy@gmail.com>
2025-08-03 14:02:10 -0400

    Update index.php

--- cats/index.php
+++ cats/index.php
@@ -18,17 +18,15 @@
     <div id="container">
       <div class="dcordchannel">Cat Wall<div>Welcome to to the wall of cats, I'll add as many photos I take here of my cats :3</div></div>
       <div class='dcord2'>
-
         <?php 
         $dirname = "./!f/";
         $images = glob($dirname."*.jpg");
           
         foreach($images as $image) {
-              echo '<div><user style='--icon: url('/!f/8730cfd2557617a015a8cb776317f2a8.jpeg')'>Ashlyn</user></div>
-      <div><img src="'.$image.'"></div>';
+              echo "<div><user style='--icon: url(/!f/8730cfd2557617a015a8cb776317f2a8.jpeg)'>NAME HERE</user></div>
+<div><img src="'.$image.'"></div>";
         }
         ?>
-      
       </div>
     </div>  
   </body>
